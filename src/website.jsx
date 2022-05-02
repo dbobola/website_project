3adb9466c5da69c4ac6584b2f6364ed2f86a872b
dbobola <dbeale@ucsd.edu>
2022-05-01 19:47:02 -0700

    Created javascript files

---
+++ src/website.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Website() {
+    return (
+        <div>
+            <h>Hello</h>
+            <p>This is some writing. I enjoy writing words because words are fun.</p>
+        </div>
+    );
+}
+
+export default Website;
