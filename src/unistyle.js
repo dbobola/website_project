ce226a83095decf7b77d55fe4fa894cbfe1a3800
dbobola <dbeale@ucsd.edu>
2022-05-16 16:08:27 -0700

    started code for mobile navbar

--- src/unistyle.js
+++ src/unistyle.js
@@ -3,8 +3,11 @@ import { createGlobalStyle } from "styled-components";
 const Unistyle = createGlobalStyle`
     * {
         margin: 0;
-        background-color: #e6f6fc;
         font-family: Arial, Helvetica, sans-serif;
+    }
+    
+    body {
+        background-color: #e6f6fc;
     }
 `;
 
