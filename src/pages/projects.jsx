d43933748517b8650ae36ac9496a1075547f68f4
dbobola <dbeale@ucsd.edu>
2022-05-20 16:30:12 -0700

    add growing / shrinking button to projects

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Circles from "../../images/circles_0001.png";
 import Cityscape from "../../images/cityscape_0001.png";
@@ -15,48 +15,93 @@ const StyledH1 = styled.h1`
     margin-bottom: 20px;
 `;
 
-const StyledP = styled.p`
+const PageOverviewStyling = styled.p`
     font-size: 25px;
+    margin-bottom: 100px;
+`;
+
+const ProjectDescriptionStyling = styled.p`
+    font-size: 25px;
+    margin-bottom: 20px;
 `;
 
 const StyledImg = styled.img`
     border-style: solid;
     border-color: black;
-    margin-bottom: 30px;
+    margin-bottom: 80px;
 `;
 
+const ButtonStyling = styled.button`
+    border: 3px solid #048c24;
+    border-radius: 100px;
+    background-color: #90fca9;
+    color: #048c24;
+    padding: 20px 40px;
+    transition-duration: 1s;
+    transition-property: font-size;
+    font-size: ${(props) => props.inputFont}px;
+    margin-top: 100px;
+    margin-bottom: 100px;
+    margin-left: auto;
+    margin-right: auto;
+    display: block;
+`;
+
+let increase = true;
+
 function Projects() {
+    const [size, setSize] = useState(20);
+    let change = 20;
+    let buttonText = "BREATHE IN";
+
+    if (size <= 20) {
+        increase = true;
+    }
+    if (size >= 180) {
+        increase = false;
+    }
+
+    if (increase) {
+        change = 20;
+        buttonText = "BREATHE IN";
+    }
+    if (!increase) {
+        change = -20;
+        buttonText = "BREATHE OUT";
+    }
+
     return (
         <StyledDiv>
             <StyledH1>Projects</StyledH1>
-            <StyledP>
+            <PageOverviewStyling>
                 Here you can find some projects that I am working on.
-                <br />
-                <br />
-                <br />
-            </StyledP>
+            </PageOverviewStyling>
             <StyledH1>Processing:</StyledH1>
-            <StyledP>
+            <ProjectDescriptionStyling>
                 Growing and Shrinking Bubbles
-                <br />
-            </StyledP>
+            </ProjectDescriptionStyling>
             <StyledImg
                 src={Circles}
                 alt="500 colorful bubbles"
                 width="750"
                 height="450"
             />
-            <br />
-            <StyledP>
+            <ProjectDescriptionStyling>
                 Scrolling Cityscape
-                <br />
-            </StyledP>
+            </ProjectDescriptionStyling>
             <StyledImg
                 src={Cityscape}
                 alt="Cityscape with sunset"
                 width="750"
                 height="450"
             />
+            <StyledH1>Miscellaneous</StyledH1>
+            <ButtonStyling
+                inputFont={size}
+                onClick={() => setSize(size + change)}
+            >
+                {buttonText}
+            </ButtonStyling>
         </StyledDiv>
     );
 }
