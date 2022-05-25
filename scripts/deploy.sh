103f761b7a918b09b0139a5022451d69b17e8a63
ssaberan <ssaberan96@gmail.com>
2022-05-24 21:39:54 -0400

    allow deployment only on main branch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,10 +2,14 @@
 
 set -e
 
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+if [ "$BRANCH" != "main" ]; then
+    echo 'You are on the wrong branch! Switch to main branch and try again.';
+    exit 1;
+fi
+
 rm -rf dist
-
 npm run build
-
 cd dist
 
 git init
