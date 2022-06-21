0dac7145ec72b33120a102772df629c5b7a0b1f5
dollarbeale <dbeale@ucsd.edu>
2022-06-21 16:34:53 -0700

    add custom scrollbar

--- src/unistyle.js
+++ src/unistyle.js
@@ -10,6 +10,28 @@ const Unistyle = createGlobalStyle`
         background: #241f24;
         color: white;
         margin: 90px 0 0 110px;
+
+        @media screen and (max-width: 900px), (max-height: 640px) {
+            margin: 0 0 0 0;
+        }
+
+        ::-webkit-scrollbar {
+            width: 25px;
+            background: #382d3b;
+        }
+    
+        ::-webkit-scrollbar-track {
+            border-radius: 5px;
+        }
+        
+        ::-webkit-scrollbar-thumb {
+            background: #03ff3d; 
+            border-radius: 5px;
+        }
+    
+        ::-webkit-scrollbar-thumb:hover {
+            background: #53fc7a; 
+        }
     }
 `;
 
