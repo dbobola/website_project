bb6fdddd0054a79ea678d6522f27968bd020c967
dollarbeale <dbeale@ucsd.edu>
2022-06-28 12:05:18 -0700

    create 3 reflexive columns for images

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
-import Flowers from "../../images/Bright Flowers.jpg";
-import Sparkles from "../../images/Blue Sparkles.gif";
-import Handsome from "../../images/Handsome Avatar Squidward.png";
+import Blue from "../../images/Blue Square.jpg";
+import Green from "../../images/Green Square.jpg";
+import Red from "../../images/Red Square.jpg";
 
-const StyledDiv = styled.div`
+const GalleryContainer = styled.div`
     margin: 0 70px 25px 70px;
 
     @media screen and (max-width: 900px), (max-height: 640px) {
@@ -12,102 +12,84 @@ const StyledDiv = styled.div`
     }
 `;
 
-const StyledH1 = styled.h1`
-    font-size: 50px;
+const MainHeader = styled.h1`
+    font-size: 70px;
     margin-bottom: 20px;
+    text-align: center;
+
+    @media screen and (max-width: 900px), (max-height: 640px) {
+        font-size: 50px;
+    }
 `;
 
-const StyledP = styled.p`
-    font-size: 25px;
-    margin-bottom: 30px;
+const GalleryDescription = styled.p`
+    font-size: 40px;
+    margin-bottom: 20px;
+    line-height: 1.5;
+    text-align: center;
+
+    @media screen and (max-width: 900px), (max-height: 640px) {
+        font-size: 25px;
+    }
+`;
+
+const ColumnContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+const ImageColumn = styled.div`
+    paddding: 10px;
+    margin: 10px;
+    flex: 30%;
+    @media screen and (max-width: 900px), (max-height: 640px) {
+        flex: 100%;
+    }
 `;
 
 const StyledImg = styled.img`
     border-style: solid;
     border-color: black;
-    margin-bottom: 30px;
-`;
-
-const StyledButt = styled.button`
-    border-style: solid;
-    background-color: purple;
-    color: white;
-    font-size: 25px;
-    margin-top: 30px;
-    margin-bottom: 30px;
+    margin-bottom: 20px;
+    margin-left: auto;
+    margin-right: auto;
+    display: block;
+    width: auto;
+    height: auto;
+    max-width: 100%;
 `;
 
 function Gallery() {
-    const [flowerZoom, setFlowerZoom] = useState(false);
-    let flowerWidth = "800";
-    let flowerHeight = "500";
-    let flowerInOption = "In";
-    if (flowerZoom) {
-        flowerWidth = "1600";
-        flowerHeight = "1000";
-        flowerInOption = "Out";
-    }
-
-    const [sparkleZoom, setSparkleZoom] = useState(false);
-    let sparkleWidth = "400";
-    let sparkleHeight = "400";
-    let sparkleInOption = "In";
-    if (sparkleZoom) {
-        sparkleWidth = "800";
-        sparkleHeight = "800";
-        sparkleInOption = "Out";
-    }
-
-    const [squidZoom, setSquidZoom] = useState(false);
-    let squidWidth = "350";
-    let squidHeight = "250";
-    let squidInOption = "In";
-    if (squidZoom) {
-        squidWidth = "700";
-        squidHeight = "500";
-        squidInOption = "Out";
-    }
-
     return (
-        <StyledDiv>
-            <StyledH1>Gallery</StyledH1>
-            <StyledP>
-                Please enjoy this gallery containing lots of beautiful images.
-            </StyledP>
-            <br />
-            <StyledButt onClick={() => setFlowerZoom(!flowerZoom)}>
-                Click to Zoom {flowerInOption}
-            </StyledButt>
-            <br />
-            <StyledImg
-                src={Flowers}
-                alt="Bright Flowers"
-                width={flowerWidth}
-                height={flowerHeight}
-            />
-            <br />
-            <StyledButt onClick={() => setSparkleZoom(!sparkleZoom)}>
-                Click to Zoom {sparkleInOption}
-            </StyledButt>
-            <br />
-            <StyledImg
-                src={Sparkles}
-                alt="Moving blue sparkles"
-                width={sparkleWidth}
-                height={sparkleHeight}
-            />
-            <br />
-            <StyledButt onClick={() => setSquidZoom(!squidZoom)}>
-                Click to Zoom {squidInOption}
-            </StyledButt>
-            <br />
-            <StyledImg
-                src={Handsome}
-                alt="Handsome avatar Squidward"
-                width={squidWidth}
-                height={squidHeight}
-            />
-        </StyledDiv>
+        <GalleryContainer>
+            <MainHeader>Check it out!</MainHeader>
+            <GalleryDescription>
+                Here is an assortment of images relating to my work.
+            </GalleryDescription>
+            <ColumnContainer>
+                <ImageColumn>
+                    <StyledImg src={Blue} alt="Blue" />
+                    <StyledImg src={Green} alt="Green" />
+                    <StyledImg src={Red} alt="Red" />
+                    <StyledImg src={Blue} alt="Blue" />
+                    <StyledImg src={Green} alt="Green" />
+                </ImageColumn>
+                <ImageColumn>
+                    <StyledImg src={Green} alt="Green" />
+                    <StyledImg src={Red} alt="Red" />
+                    <StyledImg src={Blue} alt="Blue" />
+                    <StyledImg src={Green} alt="Green" />
+                    <StyledImg src={Red} alt="Red" />
+                </ImageColumn>
+                <ImageColumn>
+                    <StyledImg src={Red} alt="Red" />
+                    <StyledImg src={Blue} alt="Blue" />
+                    <StyledImg src={Green} alt="Green" />
+                    <StyledImg src={Red} alt="Red" />
+                    <StyledImg src={Blue} alt="Blue" />
+                </ImageColumn>
+            </ColumnContainer>
+        </GalleryContainer>
     );
 }
 
